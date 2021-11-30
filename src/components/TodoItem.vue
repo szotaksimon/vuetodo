73343410ccb7015bebe89785f4a81524ab57908c
Palocz Istvan <istvan@palocz.hu>
2021-11-30 14:31:44 +0100

    Add Edit and Save base functionality

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,32 @@
 <template>
   <li>
-      {{title}}
-      <button>Edit</button>
+      <div v-if="!edit">
+        {{title}}
+        <button @click="Edit">Edit</button>
+      </div>
+      <div v-if="edit">
+          <input type="text">
+          <button @click="Save">Save</button>
+      </div>
   </li>
 </template>
 
 <script>
 export default {
     props: ['title'],
+    data() {
+        return {
+            edit: false,
+        }
+    },
+    methods: {
+        Edit() {
+            this.edit = true
+        },
+        Save() {
+            this.edit = false
+        }
+    }
 }
 </script>
 
